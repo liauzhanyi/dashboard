37726fc47f4c21b8b9c1d10fc90782db9381b699
Liau Zhan Yi <liauzhanyi@gmail.com>
2023-05-24 21:52:44 +0800

    add labels

--- gui.py
+++ gui.py
@@ -6,7 +6,11 @@ root.geometry("1000x1000")
 root.title("Interactive Dashboard")
 
 ## Create Label Widget
-myLabel = Label(root, text = "TELEOS-2")
-myLabel.pack() # Shove it to screen
+## grid row and column is relative not absolute
+label1 = Label(root, text = "TELEOS-2").grid(row=0, column=1)
+label2 = Label(root, text = "Satellite:").grid(row=0, column=0) 
+
+## Shove it to screen
+# myLabel.pack() 
 
 root.mainloop()
